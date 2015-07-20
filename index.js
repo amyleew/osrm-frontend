c7d7579c942a300a7f6d544a2ffdb9e8128b0cd9
Aaron Lidman <aaronlidman@gmail.com>
2015-07-20 16:56:27 -0400

    Require multiple points

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ var control = L.Routing.control({
 
 // set viewOptions waypoints to update with map clicked waypoints after control
 
-if (viewOptions.waypoints) {
+if (viewOptions.waypoints.length > 1) {
   control.spliceWaypoints(0, 1, viewOptions.waypoints[0].latLng);
   control.spliceWaypoints(control.getWaypoints().length - 1, 1, viewOptions.waypoints[1].latLng);
 }
