9e28d90907e2e60f1ec2e653dd68cdf5e97004ce
amyleewalton <amylee@mapbox.com>
2015-08-06 18:31:16 -0400

    framework for homepage

--- readme.md
+++
@@ -1,1 +0,0 @@
-In order to run this, add `window.localStorage.setItem('mapbox_access_token', 'YOUR TOKEN')`
--- src/localization.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-module.exports = {
-  en: require('../i18n/en.js'),
-  de: require('../i18n/de.js')
-};
